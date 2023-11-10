244cab5552774fcbdb316ed3cc980002f6367c31
Ashish906 <ashish.cse.gu@gmail.com>
2023-11-10 18:45:12 +0600

    Work on table pagination

--- src/components/ReviewsToRemove.jsx
+++ src/components/ReviewsToRemove.jsx
@@ -1,9 +1,25 @@
 'use client'
 
 import Link from 'next/link'
-import { Button, TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Paper, Grid, Rating } from '@mui/material'
+import {
+    Button,
+    TableContainer,
+    Table,
+    TableHead,
+    TableBody,
+    TableRow,
+    TableCell,
+    Paper,
+    Grid,
+    Rating,
+    Stack,
+    Typography,
+    Select,
+    MenuItem
+} from '@mui/material'
 import { tableCellClasses } from '@mui/material/TableCell'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { ChevronLeft, ChevronRight } from '@mui/icons-material'
 
 import { styled } from '@mui/material/styles'
 
@@ -90,9 +106,32 @@ export default function () {
         },
     ]
 
+    const paginateButtons = [
+        {
+            value: 1,
+            isActive: true
+        },
+        {
+            value: 2,
+            isActive: false
+        },
+        {
+            value: 3,
+            isActive: false
+        },
+        {
+            value: 4,
+            isActive: false
+        },
+        {
+            value: 5,
+            isActive: false
+        }
+    ]
+
     return (
         <ThemeProvider theme={theme}>
-            <Grid container>
+            <Grid container minHeight='100vh'>
                 <Grid item xs={12} sx={{ backgroundColor: 'rgba(134, 147, 127, 0.2)' }}>
                     <Paper sx={{ mx: '1.5rem', my: '1.75rem', boxShadow: 'none' }}>
                         <TableContainer>
@@ -126,6 +165,49 @@ export default function () {
                                     }
                                 </TableBody>
                             </Table>
+                            <Stack justifyContent='space-between' direction='row' sx={{ mx: '2.19rem', my: '2.57rem' }}>
+                                <Stack justifyContent='space-between' direction='row' spacing={1}>
+                                    <Typography fontSize='1.13rem' fontWeight='500'>Show rows</Typography>
+                                    <Select
+                                        labelId="demo-customized-select-label"
+                                        id="demo-customized-select"
+                                        value={10}
+                                        sx={{ height: '1.81rem', width: '3.25rem', fontSize: '0.875rem' }}
+                                    >
+                                        <MenuItem value={10}>10</MenuItem>
+                                        <MenuItem value={20}>20</MenuItem>
+                                        <MenuItem value={30}>30</MenuItem>
+                                    </Select>
+                                </Stack>
+                                <Stack justifyContent='space-between' direction='row' spacing={1}>
+                                    <Button sx={{ color: 'black', borderRadius: 1, border: '1px solid #86937F', minWidth: '0rem', width: '2.5rem', height: '2.5rem' }}>
+                                        <ChevronLeft/>
+                                    </Button>
+                                    {
+                                        paginateButtons.map((item) => {
+                                            return (
+                                                <Button key={item.value} variant={item.isActive ? 'contained' : 'outlined'}
+                                                    sx={{
+                                                        minWidth: '0rem',
+                                                        width: '2.5rem',
+                                                        height: '2.5rem',
+                                                        border: '1px solid #86937F',
+                                                        backgroundColor: `${item.isActive ? '#86937F' : 'none'}`,
+                                                        fontSize: '0.875rem',
+                                                        fontWeight: '600',
+                                                        color: 'black'
+                                                    }}
+                                                >
+                                                    {item.value}
+                                                </Button>
+                                            )
+                                        })
+                                    }
+                                    <Button sx={{ color: 'black', borderRadius: 1, border: '1px solid #86937F', minWidth: '0rem', width: '2.5rem', height: '2.5rem' }}>
+                                        <ChevronRight/>
+                                    </Button>
+                                </Stack>
+                            </Stack>
                         </TableContainer>
                     </Paper>
                 </Grid>
